3b468e3a795614554c684211283eaa6a5d034fd6
Gilson Fábio <gilsonfabio@gmail.com>
2022-07-28 11:19:00 -0300

    Four commit - mudanca de porta

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'https://sindicatobackend.herokuapp.com' });
+const api = axios.create({baseURL: 'https://sindicatobackend.herokuapp.com:3333' });
 
 export default api;
