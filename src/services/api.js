f92ad15b99232d3f1615b8a976eac1b5f5d3719e
Gilson Fábio <gilsonfabio@gmail.com>
2022-08-10 14:38:42 -0300

    Alteração do link do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'https://sindicatobackend.herokuapp.com' });
-
+const api = axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com' });
 export default api;
